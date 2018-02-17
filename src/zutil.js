7cbe1753f93851422544b032a44c979a5a67240b
mayama <root@lab002132-basix-tester>
2018-02-17 09:39:13 +0900

    Corrected zutil.prettyPrint

--- src/zutil.js
+++ src/zutil.js
@@ -11,7 +11,7 @@ var _i = (depth) => {
 
 var _isSimpleType = (x) => {
 	var t = typeof(x)
-	if(t == 'string' || t == 'bool' || t == 'number' || t == 'undefined' || t == 'null') {
+	if(t == 'string' || t == 'boolean' || t == 'number' || t == 'undefined' || t == 'null') {
 		return true
 	} else if(t == 'function') {
 		if(x.__original_data__) return false
@@ -21,51 +21,61 @@ var _isSimpleType = (x) => {
 	}
 }
 
-var _prettyPrintDictElements = (dict, depth) => {
+var _prettyPrintDictElements = (dict, depth, visited) => {
 	var keys = Object.keys(dict)
 	var printableKeys = _.filter(keys, (key) => !key.startsWith("_"))
 	var items = _.chain(printableKeys).map((key) => { 
 			var val = dict[key]
-			return _i(depth+1) + key + ": " + (_isSimpleType(val) ? util.inspect(val) : _prettyPrint(val, depth+1, true))  
+			return _i(depth+1) + key + ": " + (_isSimpleType(val) ? util.inspect(val) : _prettyPrint(val, depth+1, true, visited))  
 		}
 	).value()
 	if(items.length != keys.length) {
-		items.push(_i(depth+1) + "... ABRIDGED ...")
+		items.push(_i(depth+1) + "... ATTRS WITH NAMES STARTING WITH UNDERSCORE OMITTED ...")
 	}
 	return items.join(",\n")
 }
 
-var _prettyPrintArrayElements = (array, depth) => {
+var _prettyPrintArrayElements = (array, depth, visited) => {
 	return _.join(_.map(array, (e) => { 
-		return _prettyPrint(e, depth+1, false)
+		return _prettyPrint(e, depth+1, false, visited)
 	}),',\n')
 }
 
-var _prettyPrint = (x, depth=0, same_line) => {
+var _prettyPrint = (x, depth=0, same_line, visited) => {
 	var front_indent = same_line ? '' : _i(depth)
 	if(x === undefined) {
 		return front_indent + 'undefined'
 	} else if(x === null) {
 		return front_indent + 'null'
 	} else if(Array.isArray(x)) {
-		return front_indent + "[\n" + _prettyPrintArrayElements(x, depth) + "\n" + _i(depth) + "]"
+		return front_indent + "[\n" + _prettyPrintArrayElements(x, depth, visited) + "\n" + _i(depth) + "]"
 	} else if(typeof x == 'object') {
+		/*
 		if(util.inspect(x).indexOf('[Circular]') >= 0) {
 			return '[Object]'
 		}
-		return front_indent + "{\n" + _prettyPrintDictElements(x, depth) + "\n" + _i(depth) + "}"
+		*/
+		if(visited.includes(x)) {
+			return '[CircularReference]'
+		}
+		visited.push(x)
+		return front_indent + "{\n" + _prettyPrintDictElements(x, depth, visited) + "\n" + _i(depth) + "}"
 	} else if(typeof x == 'function' && x.__original_data__) {
 		var isArr = Array.isArray(x.__original_data__) 
 		return front_indent + x.__name__ + 
 			(isArr ? "([\n" : "({\n") + 
-			(isArr ? _prettyPrintArrayElements(x.__original_data__, depth) : _prettyPrintDictElements(x.__original_data__, depth)) + "\n" +
+			(isArr ? _prettyPrintArrayElements(x.__original_data__, depth) : _prettyPrintDictElements(x.__original_data__, depth, visited)) + "\n" +
 			_i(depth) + (isArr ? "])" : "})")
 	} else {
 		return _i(depth) + util.inspect(x)
 	}
 }
 
+var prettyPrint = (x, depth=0, same_line) => {
+	return _prettyPrint(x, depth, same_line, [])
+}
+
 module.exports = {
-	prettyPrint: _prettyPrint,
+	prettyPrint: prettyPrint,
 }
 
